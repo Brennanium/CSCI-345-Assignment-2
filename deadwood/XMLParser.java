523f871d93c348fa64a28d013bb89b2659d30ffa
Brennan Drew <nannerb156@gmail.com>
2020-11-07 16:58:13 -0800

    working on readAreaData

--- deadwood/XMLParser.java
+++ deadwood/XMLParser.java
@@ -43,46 +43,51 @@ public class XMLParser {
       Element root = d.getDocumentElement();
       
       NodeList sets = root.getElementsByTagName("set");
+      Node set;
       NodeList trailer = root.getElementsByTagName("trailer");
       NodeList office = root.getElementsByTagName("office");
+
+      ArrayList<Area> areas = new ArrayList<Area>();
+      Area area;
+      String areaName;
+      NodeList parts;
+      Node part;
+      ArrayList<Role> roles = new ArrayList<Role>();
+      Role role;
       
       for (int i=0; i<sets.getLength();i++){
-            
-         System.out.println("Printing information for book "+(i+1));
          
          //reads data from the nodes
-         Node set = sets.item(i);
-         String bookCategory = set.getAttributes().getNamedItem("category").getNodeValue();
-         System.out.println("Category = "+bookCategory);
+         set = sets.item(i);
+         areaName = set.getAttributes().getNamedItem("name").getNodeValue();
          
          //reads data
                                     
-         NodeList children = set.getChildNodes();
-         
-         for (int j=0; j< children.getLength(); j++){
+         parts = set.getAttributes().getNamedItem("parts").getChildNodes();
+         for (int j=0; j< parts.getLength(); j++){
             
-            Node sub = children.item(j);
+            part = parts.item(j);
             
-            if("title".equals(sub.getNodeName())){
-               String bookLanguage = sub.getAttributes().getNamedItem("lang").getNodeValue();
+            if("title".equals(part.getNodeName())){
+               String bookLanguage = part.getAttributes().getNamedItem("lang").getNodeValue();
                System.out.println("Language = "+bookLanguage);
-               String title = sub.getTextContent();
+               String title = part.getTextContent();
                System.out.println("Title = "+title);
                
             }
             
-            else if("author".equals(sub.getNodeName())){
-               String authorName = sub.getTextContent();
+            else if("author".equals(part.getNodeName())){
+               String authorName = part.getTextContent();
                System.out.println(" Author = "+authorName);
                
             }
-            else if("year".equals(sub.getNodeName())){
-               String yearVal = sub.getTextContent();
+            else if("year".equals(part.getNodeName())){
+               String yearVal = part.getTextContent();
                System.out.println(" Publication Year = "+yearVal);
                
             }
-            else if("price".equals(sub.getNodeName())){
-               String priceVal = sub.getTextContent();
+            else if("price".equals(part.getNodeName())){
+               String priceVal = part.getTextContent();
                System.out.println(" Price = "+priceVal);
                
             }
