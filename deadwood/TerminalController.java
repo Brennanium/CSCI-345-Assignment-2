c0014c6b60d85ca136935855eccd8f0cf1bc4b4c
thannareet <63220204+thannareet@users.noreply.github.com>
2020-11-08 16:16:33 -0800

    Upload the terminal controller

--- deadwood/TerminalController.java
+++ deadwood/TerminalController.java
@@ -4,8 +4,11 @@
 package deadwood;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import deadwood.model.*;
+import deadwood.model.areas;
 
 public class TerminalController {
     private ActionManager model;
@@ -21,22 +24,34 @@ public class TerminalController {
         Scanner sc = new Scanner(System.in);
         if(sc.hasNext())
             userInput = sc.nextLine();
-        while(!userInput.equals("quit") && sc.hasNext()){
-            
-            dealWithUserInput(userInput);
-            
+        while(!userInput.equals("end") && sc.hasNext()){           
+            dealWithUserInput(userInput);            
         }
     }
-    
+
     private void dealWithUserInput(String input) {
-        if(input.equals("who")){
+        if(input.equals("Active player?")){
             model.getCurrentPlayer();
-        }
-        if(input.matches("work \\w*")){
-            model.takeRole("name of the role");
+        } else if(input.equals("who")){
+            model.getNonPlayer();
+        } else if(input.equals("where")){
+            model.getCurrentArea();
+        } else if(input.equals("act")){
+            model.act();
+        } else if(input.matches("move(.*)")){
+            String place = input;
+            String command = place.substring(0,5);
+            String name = place.substring(6, place.length()-1);
+            model.move("name");
+        } else if(input.matches("work(.*)")){
+            String place = input;
+            String command = place.substring(0,5);
+            String name = place.substring(6, place.length()-1);
+            model.takeRole("name");
         }
     }
        
-    //public static void main()
+    public static void main(String args[]){
+    }
 }
 
